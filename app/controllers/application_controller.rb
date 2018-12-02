206b1dfe4d81a63955ada91fec2d820dd857c162
Raymond Fallon <Raymond.Fallon@pennmedicine.upenn.edu>
2018-12-02 15:03:05 -0500

    Move EmergencyExit To Separate Module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,7 @@
+require 'emergency_exit'
+
 class ApplicationController < ActionController::Base
+  include EmergencyExit
+
   before_action :clear_cache, :redirect_away_if_emergency
-
-  # Keep Browswer Cache Clear of Site Data
-  # This prevents someone from simply clicking the "Back" button and
-  # seeing the site after the Emergency Exit is used
-  def clear_cache
-    response.headers['Cache-Control'] = 'no-cache, no-store'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
-  end
-
-  # Prevents any page on the site from rendering until the Emergency Exit cookie has expired
-  def redirect_away_if_emergency
-    if session[:exited]
-      if Time.zone.now > session[:expires_at]
-        session.destroy
-        return true
-      end
-      redirect_to 'https://youtube.com'
-    end
-  end
 end
